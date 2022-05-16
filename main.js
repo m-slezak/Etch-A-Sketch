50eb979793258daddd708a4a86205aa9bd1bd707
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-05-16 12:23:00 +0200

    div styles

--- main.js
+++ main.js
@@ -5,8 +5,12 @@ let divs;
 
 for( let i = 0; i<16; i++){
     divs = document.createElement('div');
-    divContainer.append(divs)
+    divs.classList.add('.div-element')
+    divs.setAttribute('style', 'height: 100px', 'width: 100px', 'border: solid 3px black')
+    divContainer.appendChild(divs)
+    divs.innerHTML = "hello"
 }
+
 
 
 
