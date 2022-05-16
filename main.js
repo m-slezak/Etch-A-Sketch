72849d1cb2258be5ea58f097fa495bb1709416a1
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-05-16 12:04:56 +0200

    16 divs created

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 
 
 const divContainer = document.querySelector('#container');
+let divs;
+
+for( let i = 0; i<16; i++){
+    divs = document.createElement('div');
+    divContainer.append(divs)
+}
 
 
 
