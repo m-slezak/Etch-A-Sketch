bdf0e10a98d9bb06060aafa2de87ab085dd719a2
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-05-17 14:42:20 +0200

    16x16grid created

--- main.js
+++ main.js
@@ -3,12 +3,11 @@
 const divContainer = document.querySelector('#container');
 let divs;
 
-for( let i = 0; i<16; i++){
+for(let i = 0; i < 256; i++){
     divs = document.createElement('div');
     divs.classList.add('.div-element')
-    divs.setAttribute('style', 'height: 100px', 'width: 100px', 'border: solid 3px black')
+    divs.setAttribute('style', 'border: solid 3px black')
     divContainer.appendChild(divs)
-    divs.innerHTML = "hello"
 }
 
 
