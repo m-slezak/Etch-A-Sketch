454777ba087e84a851819a932dec9b955854b85d
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-05-17 22:40:59 +0200

    more styles

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-
-
 const divContainer = document.querySelector('#container');
 let div;
+const btn = document.querySelector('#btn');
+const input = document.querySelector('#input')
+const numberOfSquares = (input.value * input.value);
+
+btn.addEventListener('click', () =>{
+    for(let i = 0; i < numberOfSquares; i++){
+        div = document.createElement('div');
+        div.classList.add('div-element')
+        div.setAttribute('style', 'border: solid 3px black')
+        divContainer.appendChild(div)
+    }
+
+})
+
 
 for(let i = 0; i < 256; i++){
     div = document.createElement('div');
