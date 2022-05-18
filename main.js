d9f256827904e1ce11c5e7a27c1819c3cd086c88
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-05-18 15:44:14 +0200

    . prettierrc, creating squares

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
-const divContainer = document.querySelector('#container');
+const divContainer = document.querySelector("#container");
 let div;
-const btn = document.querySelector('#btn');
-const input = document.querySelector('#input')
-const numberOfSquares = (input.value * input.value);
+const btn = document.querySelector("#btn");
+const input = document.querySelector("#input");
+const numberOfSquares = input.value * input.value;
+const board = document.querySelectorAll(".div-element");
 
-btn.addEventListener('click', () =>{
-    for(let i = 0; i < numberOfSquares; i++){
-        div = document.createElement('div');
-        div.classList.add('div-element')
-        div.setAttribute('style', 'border: solid 3px black')
-        divContainer.appendChild(div)
-    }
+const inputValue = input.value;
 
-})
+allDivs.forEach((div) =>
+  div.addEventListener("mouseenter", () => {
+    div.style.backgroundColor = "black";
+  })
+);
 
-
-for(let i = 0; i < 256; i++){
-    div = document.createElement('div');
-    div.classList.add('div-element')
-    div.setAttribute('style', 'border: solid 3px black')
-    divContainer.appendChild(div)
+function changeSize(inputValue) {
+  makeSquare(inputValue);
 }
-
-const allDivs = document.querySelectorAll('.div-element');
-
-allDivs.forEach(div => div.addEventListener('mouseenter', () =>{
-    div.style.backgroundColor = 'black';
-}))
-
-
-
+function makeSquare(size) {
+  let board = document.querySelectorAll(".div-element");
+  board.forEach((div) => div.remove());
+  divContainer.style.gridTemplateColumns = `repeat(${size}, 1fr`;
+  divContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  for (let i = 0; i < `${size * size}`; i++) {
+    div = document.createElement("div");
+    div.classList.add("div-element");
+    div.setAttribute("style", "border: solid 1px black");
+    divContainer.appendChild(div);
+  }
+}
+btn.addEventListener("click", makeSquare);
