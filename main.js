73c517867e84ae6fa79a0afa79efa34472ac304a
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-05-19 20:28:13 +0200

    Make squares, drawing working

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
+const inputField = document.querySelector("#input");
 const divContainer = document.querySelector("#container");
-let div;
-const btn = document.querySelector("#btn");
-const input = document.querySelector("#input");
-const numberOfSquares = input.value * input.value;
-const board = document.querySelectorAll(".div-element");
+let size;
 
-const inputValue = input.value;
+// const btn = document.querySelector("#btn");
 
-allDivs.forEach((div) =>
-  div.addEventListener("mouseenter", () => {
-    div.style.backgroundColor = "black";
-  })
-);
-
-function changeSize(inputValue) {
-  makeSquare(inputValue);
+function changeSize(input) {
+  size = inputField.value;
+  if (size >= 2 && size <= 100) {
+    makeSquare(size);
+  } else {
+    console.log("Too many, too less squares");
+  }
 }
 function makeSquare(size) {
   let board = document.querySelectorAll(".div-element");
-  board.forEach((div) => div.remove());
-  divContainer.style.gridTemplateColumns = `repeat(${size}, 1fr`;
+  board.forEach((square) => square.remove());
+  divContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   divContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
   for (let i = 0; i < `${size * size}`; i++) {
-    div = document.createElement("div");
-    div.classList.add("div-element");
-    div.setAttribute("style", "border: solid 1px black");
-    divContainer.appendChild(div);
+    let board = document.querySelectorAll(".div-element");
+    const square = document.createElement("div");
+    square.classList.add("div-element");
+    square.setAttribute("style", "border: solid 1px black");
+    board.forEach((square) =>
+      square.addEventListener("mouseenter", () => {
+        square.setAttribute("style", "background-color: black");
+      })
+    );
+
+    divContainer.appendChild(square);
   }
 }
-btn.addEventListener("click", makeSquare);
