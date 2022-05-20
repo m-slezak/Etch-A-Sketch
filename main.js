32de50868e05b050e5e96794c763f4bf7e3c88d5
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-05-20 05:09:21 +0200

    iterating throug radio buttons

--- main.js
+++ main.js
@@ -2,33 +2,47 @@ const inputField = document.querySelector("#input");
 const divContainer = document.querySelector("#container");
 let size;
 
+const radioBtns = document.querySelectorAll('input[name="changeColor"]');
+const btn2 = document.querySelector("#btn-2");
+
+btn2.addEventListener("click", () => {
+  let selectedColor;
+  for (const radioBtn of radioBtns) {
+    if (radioBtn.checked) {
+      selectedColor = radioBtn.value;
+      console.log(selectedColor);
+      break;
+    }
+  }
+});
+
 // const btn = document.querySelector("#btn");
 
-function changeSize(input) {
-  size = inputField.value;
-  if (size >= 2 && size <= 100) {
-    makeSquare(size);
-  } else {
-    console.log("Too many, too less squares");
-  }
-}
-function makeSquare(size) {
-  let board = document.querySelectorAll(".div-element");
-  board.forEach((square) => square.remove());
-  divContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  divContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+// function changeSize(input) {
+//   size = inputField.value;
+//   if (size >= 2 && size <= 100) {
+//     makeSquare(size);
+//   } else {
+//     console.log("Too many, or not enough squares");
+//   }
+// }
+// function makeSquare(size) {
+//   let board = document.querySelectorAll(".div-element");
+//   board.forEach((square) => square.remove());
+//   divContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+//   divContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-  for (let i = 0; i < `${size * size}`; i++) {
-    let board = document.querySelectorAll(".div-element");
-    const square = document.createElement("div");
-    square.classList.add("div-element");
-    square.setAttribute("style", "border: solid 1px black");
-    board.forEach((square) =>
-      square.addEventListener("mouseenter", () => {
-        square.setAttribute("style", "background-color: black");
-      })
-    );
+//   for (let i = 0; i < `${size * size}`; i++) {
+//     let board = document.querySelectorAll(".div-element");
+//     const square = document.createElement("div");
+//     square.classList.add("div-element");
+//     square.setAttribute("style", "border: solid 1px black");
+//     board.forEach((square) =>
+//       square.addEventListener("mouseenter", () => {
+//         square.setAttribute("style", "background-color: black");
+//       })
+//     );
 
-    divContainer.appendChild(square);
-  }
-}
+//     divContainer.appendChild(square);
+//   }
+// }
